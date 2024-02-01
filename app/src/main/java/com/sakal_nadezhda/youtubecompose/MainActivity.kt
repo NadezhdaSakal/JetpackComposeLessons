232b071a60f5be08ac84df5649f7064a85770227
NadezhdaSakal <sakaln88@gmail.com>
2024-02-01 23:11:03 +0300

    card, box, image

--- app/src/main/java/com/sakal_nadezhda/youtubecompose/MainActivity.kt
+++ app/src/main/java/com/sakal_nadezhda/youtubecompose/MainActivity.kt
@@ -3,23 +3,70 @@ package com.sakal_nadezhda.youtubecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            newText()
+            Column {
+                listItem(name = "Radio Tapok", song = "Халхин-Гол")
+                listItem(name = "Radio Tapok", song = "Халхин-Гол")
+                listItem(name = "Radio Tapok", song = "Халхин-Гол")
+                listItem(name = "Radio Tapok", song = "Халхин-Гол")
+                listItem(name = "Radio Tapok", song = "Халхин-Гол")
+            }
         }
-    }
-
-    @Preview (showBackground = true)
-    @Composable
-    fun newText() {
-        Text(text = "Hi Compose")
     }
 }
 
+@Composable
+private fun listItem(name: String, song: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Box() {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(modifier = Modifier.padding(start = 12.dp)) {
+                    Text(text = name)
+                    Text(text = song)
+
+                }
+            }
+
+        }
+
+    }
+}
